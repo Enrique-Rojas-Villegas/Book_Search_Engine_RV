5a73e9a359bf2838ce75bf230e2de8cd941d2f95
Enoc Rojas <rojiyoyo@gmail.com>
2022-04-27 15:37:06 -0500

    Added typedef Mutations, Queries, and User, Book and input types

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,46 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        password: String!
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Book {
+        bookId: ID!
+        authors: [String]
+        description: String
+        image: String
+        link: Int
+        title: String
+    }
+
+    input InputBook {
+        bookId: String
+        authors: [String]
+        description: String
+        image: String
+        link: String
+        title: String
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String! ,email: String!, password: String!): Auth
+        saveBook(newBook: InputBook): User
+        removeBook(bookId: ID!): User
+    }
+`;
+
+module.exports = typeDefs;
